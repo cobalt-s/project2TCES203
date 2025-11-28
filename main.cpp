9fd5df3c2d76b8e32b0a7186c502c2d149abfff8
“cobalt-s” <stameycobalt@gmail.com>
2025-11-28 11:41:41 -0800

    started work on main menu code in main.cpp added a printMenu and printActionmenu still needs more work.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,33 @@
 
 void printMenu()
 {
-  std::cout << "Menu: for car" << "\n";
+  std::cout << "Menu: Car Controller" << "\n";
   std::cout << "Which mode would you like to enter?" << "\n";
   std::cout << "(1) Control with computer" << "\n";
   std::cout << "(2) Test with a file " << "\n";
+  std::cout << "(3) Quit" << "\n";
+  std::cout << "(3) Quit" << "\n";
 }
+
+void printActionMenu()
+{
+  std::cout << "\nActions" << "\n";
+  std::cout << "(L) Left" << "\n";
+  std::cout << "(R) Right" << "\n";
+  std::cout << "(B) Backward" << "\n";
+  std::cout << "(F) Forward" << "\n";
+  std::cout << "(S) Forward" << "\n";
+  std::cout << "(L) Forward" << "\n";
+  std::cout << "(E) Exit to main menu" << "\n";
+  std::cout << "Choose an action:" << "\n";
+}
+
+
 
 int main()
 {
+  int choice;
   printMenu();
+
 
 }
