d9b9a1e8e0b9f448a17ea849cda78ccd2a9a34a1
“cobalt-s” <stameycobalt@gmail.com>
2025-11-29 13:35:16 -0800

    everything works on this commit, fixed some of controller and userinterface fixed a few issues

--- Controller.cpp
+++ Controller.cpp
@@ -11,7 +11,7 @@ void Controller::run()
     int choice = 0;
     do {
         UserInterface::printMainMenu();
-        choice = getUserChoice();
+        choice = getUserChoiceInt();
 
         switch (choice) {
             case 1:
@@ -20,143 +20,91 @@ void Controller::run()
             case 2:
                 fileControl();
                 break;
-            case 3:
-                UserInterface::printInfo("Invalid Choice");
-                break;
             default:
                 UserInterface::printError("Invalid Choice");
                 break;
         }
     } while (choice!=3);
+
+    UserInterface::printInfo("Exiting Program");
 }
 
 void Controller::userControl()
 {
     UserInterface::printInfo("Entering User Control mode.");
-    UserInterface::printActionMenu();
-    // need to add more
+    char choice;
+    do {
+        UserInterface::printActionMenu();
+        choice = getUserChoiceChar();
+        Controller::moveCar(choice);
+    } while (choice != 'E');
+
+    UserInterface::printInfo("Exiting User Control mode.");
 }
 
 void Controller::fileControl()
 {
     UserInterface::printInfo("Entering File Control mode.");
-
+    //readfile function
 }
 
-int Controller::getUserChoice() {
-    std::cout << "Enter your choice: ";
+int Controller::getUserChoiceInt() {
+    std::cout << "Enter your choice (INT): ";
     int choice;
 
     if (!(std::cin >> choice)) {
         std::cin.clear();
         std::cin.ignore(10000, '\n');
         UserInterface::printError("Please enter a valid number.");
-        return -1;  // Forces main loop to show error
+        return -1;
     }
 
     return choice;
 }
 
+char Controller::getUserChoiceChar() {
+    std::cout << "Enter your choice(CHAR): ";
+    char choice;
 
-// /**
-//  * This is the action loop for the car moving.
-//  * @return 0 if it runs good, 1 if it runs bad.
-//  */
-// int actionLoop()
-// {
-//     char choice;
-//     do {
-//         printActionMenu();
-//         choice = getUserChoice();
-//
-//         if (choice == 'K') {
-//             //might want to change movecar to let you choose how many times that could be fun.
-//
-//             //this is a loop for the car.
-//             moveCar('L');
-//             moveCar('F');
-//             moveCar('L');
-//             moveCar('F');
-//             moveCar('L');
-//             moveCar('F');
-//             moveCar('S');
-//             return 0;
-//         }else {
-//             moveCar(choice);
-//         }
-//     } while (choice != 'E');
-//
-//     return 0;
-// }
+    if (!(std::cin >> choice)) {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        UserInterface::printError("Please enter a valid number.");
+        return -1;
+    }
+
+    return choice;
+}
+
+/**
+ * This function allows you to move the car in a specific direction.
+ * @param direction the direction to move the car.
+ */
+void Controller::moveCar(const char direction)
+{
+    //TODO: add log file implementation
+    //TODO: add movement implementation
+    switch (direction) {
+        case 'L':
+            std::cout << "\n" << "Moving car left" << "\n";
+            break;
+        case 'R':
+            std::cout << "\n" << "Moving car right" << "\n";
+            break;
+        case 'B':
+            std::cout << "\n" << "Moving car backwards" << "\n";
+            break;
+        case 'F':
+            std::cout << "\n" << "Moving car forwards" << "\n";
+            break;
+        case 'S':
+            std::cout << "\n" << "Moving car stop" << "\n";
+            break;
+        default:
+            std::cout << "\n" << "Invalid action" << "\n";
+            break;
+    }
+
+}
 
 
-// /**
-//  *
-//  * @param speed
-//  * @param cameraMode
-//  */
-// void readConfigFile(std::string &speed, std::string &cameraMode) {
-//     std::ifstream config("config.txt");
-//     if (!config) {
-//         std::cout << "No configuration file found; using defaults.\n";
-//         return;
-//     }
-//     std::string line;
-//     while (getline(config, line)) {
-//         // Very simple parsing: expect lines like "key=value"
-//         size_t eq = line.find('=');
-//         if (eq == std::string::npos) continue;
-//         std::string key = line.substr(0, eq);
-//         std::string value = line.substr(eq + 1);
-//         if (key == "speed") speed = value;
-//         else if (key == "camera_mode") cameraMode = value;
-//     }
-// }
-
-// char getUserChoiceChar() {
-//     char choice;
-//     std::cout << "Enter your choice: ";
-//
-//     if (!(std::cin >> choice)) {
-//         std::cin.clear();
-//         std::cin.ignore(10000, '\n');
-//         return '\0';
-//     }
-//
-//     std::cin.ignore(10000, '\n');
-//     return choice;
-// }
-
-// /**
-//  * This function allows you to move the car in a specific direction.
-//  * @param direction the direction to move the car.
-//  */
-// void moveCar(const char direction)
-// {
-//     switch (direction) {
-//         case 'L':
-//             std::cout << "Moving car left" << "\n";
-//             //TODO: add implementation to this movement.
-//             break;
-//         case 'R':
-//             std::cout << "Moving car right" << "\n";
-//             //TODO: add implementation to this movement.
-//             break;
-//         case 'B':
-//             std::cout << "Moving car backwards" << "\n";
-//             //TODO: add implementation to this movement.
-//             break;
-//         case 'F':
-//             std::cout << "Moving car forwards" << "\n";
-//             //TODO: add implementation to this movement.
-//             break;
-//         case 'S':
-//             std::cout << "Moving car stop" << "\n";
-//             //TODO: add implementation to this movement.
-//             break;
-//         default:
-//             std::cout << "Invalid action" << "\n";
-//             break;
-//     }
-//
-// }
